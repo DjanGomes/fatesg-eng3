fd199503f66bdf99bee18f0b4c19095eabac9c0d
Lucas Vicente <lucasvpc47@gmail.com>
2026-03-12 22:03:16 -0300

    Adicionando maior valor

---
+++ src/MaiorValor.java
@@ -0,0 +1,20 @@
+public class MaiorValor {
+
+    public int maiorValor() {
+        if(head == null){
+            System.out.println("A lista esta vazia!");
+        }
+
+        No auxiliar = head;
+        int maiorValor = auxiliar.getDado();
+
+        while ((auxiliar != null)){
+            if(auxiliar.getDado() > maiorValor ){
+                maiorValor = auxiliar.getDado();
+            }
+
+            auxiliar = auxiliar.getProximo();
+        }
+        return  maiorValor;
+    }
+}
