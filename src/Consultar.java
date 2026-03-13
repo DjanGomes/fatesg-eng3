b3270ddb3f3f63a5547e5116c5a832ab0fff57af
Davi Fraga <fragadavi30@gmail.com>
2026-03-12 21:26:41 -0300

    Adicionando métodos Consultar e MenorValor

---
+++ src/Consultar.java
@@ -0,0 +1,18 @@
+public class Consultar {
+
+    public void consultar() {
+        if (head == null) {
+            System.out.println("A lista está vazia!");
+        }
+
+        No aux = head;
+
+        System.out.println("Lista Encadeada: ");
+        while (aux != null) {
+
+            System.out.print(aux.getDado()+" -> ");
+            aux = aux.getProximo();
+        }
+        System.out.println("null");
+    }
+}
